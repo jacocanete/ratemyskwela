bb534057ad1f8f9deffc83317d1f0d9011133700
jacocanete <jacogagarincanete@gmail.com>
2024-03-12 21:54:24 +0800

    Update Footer component to use RateMyUni branding

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Footer } from "flowbite-react";
 
-export default function Footer() {
+export default function FooterCom() {
   return (
-    <div>
+    <div className="w-full max-w-7xl mx-auto">
       <Footer container>
-        <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+        <Footer.Copyright href="#" by="RateMyUni™" year={2022} />
         <Footer.LinkGroup>
           <Footer.Link href="#">About</Footer.Link>
           <Footer.Link href="#">Privacy Policy</Footer.Link>
