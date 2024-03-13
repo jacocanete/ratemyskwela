a28dc6e908fd6001c45cf214dd23c2af62609b58
jacocanete <jacogagarincanete@gmail.com>
2024-03-13 23:17:35 +0800

    refactor: routing in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
