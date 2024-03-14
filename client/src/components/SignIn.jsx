f81bedf5ce52b5f18c8c09161f0bf71f36053032
jacocanete <jacogagarincanete@gmail.com>
2024-03-14 11:27:49 +0800

    feat: form submission functionality to SignIn and SignUp components

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -5,7 +5,7 @@ import {
   FloatingLabel,
   Spinner,
 } from "flowbite-react";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { HiAcademicCap } from "react-icons/hi";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -18,6 +18,7 @@ import {
 export default function SignIn({ setSignIn, setShowModal }) {
   const [formData, setFormData] = useState({});
   const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const [toasts, setToasts] = useState([]);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -76,43 +77,41 @@ export default function SignIn({ setSignIn, setShowModal }) {
       <h3 className="mb-4 text-center text-xl font-medium text-gray-900 dark:text-white">
         Sign in to our platform
       </h3>
-      <div>
-        <FloatingLabel
-          id="identifier"
-          required
-          onChange={handleChange}
-          variant="outlined"
-          label="Username or Email"
-          className="dark:bg-gray-700"
-        />
-      </div>
-      <div>
-        <FloatingLabel
-          id="password"
-          type="password"
-          required
-          onChange={handleChange}
-          variant="outlined"
-          label="Password"
-          className="dark:bg-gray-700"
-        />
-      </div>
-      <div className="w-full">
-        <Button
-          gradientMonochrome="pink"
-          className="w-full"
-          onClick={handleSubmit}
-        >
-          {loading ? (
-            <>
-              <Spinner size="sm" />
-              <span className="pl-2">Loading...</span>
-            </>
-          ) : (
-            "Login to your account"
-          )}
-        </Button>
-      </div>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div>
+          <FloatingLabel
+            id="identifier"
+            required
+            onChange={handleChange}
+            variant="outlined"
+            label="Username or Email"
+            className="dark:bg-gray-700"
+          />
+        </div>
+        <div>
+          <FloatingLabel
+            id="password"
+            type="password"
+            required
+            onChange={handleChange}
+            variant="outlined"
+            label="Password"
+            className="dark:bg-gray-700"
+          />
+        </div>
+        <div className="w-full">
+          <Button gradientMonochrome="pink" className="w-full" type="submit">
+            {loading ? (
+              <>
+                <Spinner size="sm" />
+                <span className="pl-2">Loading...</span>
+              </>
+            ) : (
+              "Login to your account"
+            )}
+          </Button>
+        </div>
+      </form>
       <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
         Not registered?&nbsp;
         <a
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,15 +1,47 @@
 import { Button, Label, TextInput, FloatingLabel } from "flowbite-react";
 import { useRef, useState } from "react";
 import { HiAcademicCap } from "react-icons/hi";
+import { useDispatch } from "react-redux";
+import {
+  signUpStart,
+  signUpFailure,
+  signUpSuccess,
+} from "../redux/user/userSlice";
 
 export default function SignUp({ setSignIn }) {
   const [formData, setFormData] = useState({});
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return dispatch(signUpFailure("All fields are required."));
+    }
+    try {
+      dispatch(signUpStart());
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        bodyy: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signUpFailure(data.message));
+      }
+      if (res.ok) {
+        dispatch(signUpSuccess(data));
+        setShowModal(false);
+      }
+    } catch (error) {
+      dispatch(signUpFailure(error.message));
+    }
+  };
 
   return (
     <div className="space-y-6">
@@ -22,46 +54,45 @@ export default function SignUp({ setSignIn }) {
       <h3 className="mb-4 text-center text-xl font-medium text-gray-900 dark:text-white">
         Sign up to our platform
       </h3>
-      <div>
-        <FloatingLabel
-          id="username"
-          required
-          onChange={handleChange}
-          variant="outlined"
-          label="Username"
-          className="dark:bg-gray-700"
-        />
-      </div>
-      <div>
-        <FloatingLabel
-          id="email"
-          required
-          onChange={handleChange}
-          variant="outlined"
-          label="Email"
-          className="dark:bg-gray-700"
-        />
-      </div>
-      <div>
-        <FloatingLabel
-          id="password"
-          type="password"
-          required
-          onChange={handleChange}
-          variant="outlined"
-          label="Password"
-          className="dark:bg-gray-700"
-        />
-      </div>
-      <div className="w-full">
-        <Button
-          gradientMonochrome="pink"
-          className="w-full"
-          onClick={handleSubmit}
-        >
-          Create your account
-        </Button>
-      </div>
+      <form className="flex flex-col gap-4" onClick={handleSubmit}>
+        <div>
+          <FloatingLabel
+            id="username"
+            required
+            onChange={handleChange}
+            variant="outlined"
+            label="Username"
+            className="dark:bg-gray-700"
+          />
+        </div>
+        <div>
+          <FloatingLabel
+            id="email"
+            required
+            onChange={handleChange}
+            variant="outlined"
+            label="Email"
+            className="dark:bg-gray-700"
+          />
+        </div>
+        <div>
+          <FloatingLabel
+            id="password"
+            type="password"
+            required
+            onChange={handleChange}
+            variant="outlined"
+            label="Password"
+            className="dark:bg-gray-700"
+          />
+        </div>
+        <div className="w-full">
+          <Button gradientMonochrome="pink" className="w-full" type="submit">
+            Create your account
+          </Button>
+        </div>
+      </form>
+
       <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
         Already registered?&nbsp;
         <a
