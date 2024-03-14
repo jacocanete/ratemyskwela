3c00a73d0ff557689645f3ab5bcdf1852d6c0639
jacocanete <jacogagarincanete@gmail.com>
2024-03-14 23:30:05 +0800

    feat: university and user IDs to review model, and add rating component to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, Avatar } from "flowbite-react";
+import { Button, Card, Avatar, Rating } from "flowbite-react";
 
 export default function Home() {
   return (
@@ -17,10 +17,23 @@ export default function Home() {
           <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Cebu Institute of Technology - University
           </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
+          <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-3">
             Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+            far, in reverse chronological order. The combined value of the
           </p>
+          <Rating className="mt-2 justify-end">
+            <Rating.Star />
+            <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
+              4.95
+            </p>
+            <span className="mx-1.5 h-1 w-1 rounded-full bg-gray-500 dark:bg-gray-400" />
+            <a
+              href="#"
+              className="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white"
+            >
+              73 reviews
+            </a>
+          </Rating>
         </Card>
       </div>
     </div>
