1149ba76cd3aab7cc54170002415de2b81998c57
jacocanete <jacogagarincanete@gmail.com>
2024-03-31 02:09:14 +0800

    update: PrivacyPolicy styling

--- client/src/pages/PrivacyPolicy.jsx
+++ client/src/pages/PrivacyPolicy.jsx
@@ -3,7 +3,7 @@ import { Card } from "flowbite-react";
 
 export default function PrivacyPolicy() {
   return (
-    <div className="max-w-6xl mx-auto mt-5 mb-2 min-h-screen">
+    <div className="max-w-6xl mx-auto mt-5 mb-5 min-h-screen">
       <Card className="max-w-full flex flex-col shadow-md border-0 transition duration-300 ease-in-out p-2">
         <h2 className="text-4xl text-center font-bold">Privacy Policy</h2>
         <h3 className="text-2xl font-bold">Introduction: </h3>
