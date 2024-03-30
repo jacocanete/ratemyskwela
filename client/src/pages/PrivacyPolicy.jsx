67b28f3e520361c1d4034411b850342ca5c79b51
Ludensburger <mendozaryu08@gmail.com>
2024-03-31 00:58:53 +0800

    refactor: PrivacyPolicy and TermsOfService pages

--- client/src/pages/PrivacyPolicy.jsx
+++ client/src/pages/PrivacyPolicy.jsx
@@ -3,9 +3,52 @@ import { Card } from "flowbite-react";
 
 export default function PrivacyPolicy() {
   return (
-    <div className="max-w-6xl mx-auto mt-5 mb-5 min-h-screen">
+    <div className="max-w-6xl mx-auto mt-5 mb-2 min-h-screen">
       <Card className="max-w-full flex flex-col shadow-md border-0 transition duration-300 ease-in-out ">
-        Privacy Policy
+        <h2 className="text-4xl text-center font-bold">Privacy Policy</h2>
+        <h3 className="text-2xl font-bold">Introduction: </h3>
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-left">
+          We collect user information primarily for account management purposes.
+          As this website is a student project, the data collected is solely for
+          managing users within the platform. We do not intend to use this data
+          for any purposes beyond the website itself.
+        </p>
+
+        <h3 className="text-2xl font-bold">
+          Types of Data Collected - Personal Data:
+        </h3>
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-left">
+          We collect essential user information such as email addresses and
+          usernames for registration. Users remain anonymous when interacting
+          with the website. Additionally, the privacy of user emails and
+          usernames are strictly protected.
+        </p>
+
+        <h3 className="text-2xl font-bold">Cookies:</h3>
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-left">
+          Currently, our use of cookies is primarily for login functionality.
+          Further details on cookie usage may be provided as development
+          progresses.
+        </p>
+
+        <h3 className="text-2xl font-bold">Third-Party Services:</h3>
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-left">
+          Our website may offer Google as an alternative registration option.
+          However, aside from this feature, we do not utilize any additional
+          third-party services.
+        </p>
+
+        <h3 className="text-2xl font-bold">Changes to This Policy: </h3>
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-left">
+          While updates to the Privacy Policy may occur, they are expected to be
+          minimal given the nature of this project. Any changes will be
+          communicated transparently to users.
+        </p>
+        <br />
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-center">
+          For any questions or concerns regarding our Privacy Policy or data
+          practices, please feel free to contact us directly.
+        </p>
       </Card>
     </div>
   );
--- client/src/pages/TermsOfService.jsx
+++ client/src/pages/TermsOfService.jsx
@@ -5,7 +5,45 @@ export default function TermsOfService() {
   return (
     <div className="max-w-6xl mx-auto mt-5 mb-5 min-h-screen">
       <Card className="max-w-full flex flex-col shadow-md border-0 transition duration-300 ease-in-out ">
-        Terms of Service
+        <h2 className="text-4xl text-center font-bold">Terms of Service</h2>
+        <h3 className="text-2xl font-bold">Authenticity: </h3>
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-left">
+          Users are required to provide genuine and truthful reviews based on
+          their personal experiences. Reviews must refrain from containing any
+          false, misleading, or defamatory information.
+        </p>
+        <h3 className="text-2xl font-bold">Proprietary Rights: </h3>
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-left">
+          By submitting a review, users grant the platform permission to use and
+          share the review within the community for non-commercial purposes.
+          This license allows the platform to facilitate user interaction and
+          community engagement without engaging in any commercial exploitation.
+        </p>
+
+        <h3 className="text-2xl font-bold">No Conflicts of Interest: </h3>
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-left">
+          Users must confirm that they have no financial or other conflicts of
+          interest influencing their review.
+        </p>
+
+        <h3 className="text-2xl font-bold">Responsibility for Content: </h3>
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-left">
+          Users bear full responsibility for the content of their reviews and
+          any consequences arising from its publication.
+        </p>
+
+        <h3 className="text-2xl font-bold">Right to Remove: </h3>
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-left">
+          The platform reserves the right to delete any review that violates
+          these terms and conditions or is deemed inappropriate, offensive, or
+          in contravention of the law.
+        </p>
+
+        <br />
+        <p className="text-lg text-gray-600 dark:text-gray-400 text-balance text-center">
+          By submitting a review, users acknowledge that they have read,
+          understood, and consent to abide by these terms and conditions.
+        </p>
       </Card>
     </div>
   );
